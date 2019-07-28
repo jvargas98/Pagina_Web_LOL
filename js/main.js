af4975a5bf3315e7bc290e7f2c298e83533c6332
Jorge Vargas <jvargas18@ucol.mx>
2019-07-28 00:30:50 -0500

    Final

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function div(){
+    var val = document.getElementById('input').value;
+    var d = document.getElementById('div');
+    d.style.width = val + "px";
+    d.style.height = val + "px";
+}
